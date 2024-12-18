6ec00d71ddadb0e465dbf5430fd5583e649028fb
Rujan <grimr0833@gmail.com>
2024-12-18 09:25:06 +0545

    2nd lab

---
+++ 1st/q1.py
@@ -0,0 +1,6 @@
+# Write a program to create product category list and add items to the created
+# categories in an e-commerce store. Display items with their categories. You
+# can write program in any of the language as per your comfort. The design
+# is not required. Database is optional, you can also work using array or any
+# other data structure as per your comfort. Though if you opt to use database,
+# you can.
